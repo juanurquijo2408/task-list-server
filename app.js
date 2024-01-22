87f1118583765f1eaf3d897b2dab312a5c074336
Juan David <davidurquijo2408@gmail.com>
2024-01-22 13:29:46 -0500

    Actualización app.js
    Se cambio el método de creación de la variable tareas de const a let.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const tareas = {
+let tareas = {
   id: 123456,
   isCompleted: false,
   description: "Walk the dog",
