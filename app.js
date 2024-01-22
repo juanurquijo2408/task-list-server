a69d815e70d13eba807cb1127bd7ff861eb54297
Juan David <davidurquijo2408@gmail.com>
2024-01-22 14:02:36 -0500

    Actualización en el funcionamiento
    Se creó un archivo json para el almacenamiento de las tareas y se modificó el código para agregar la función de nueva tarea

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let tareas = {
-  id: 123456,
-  isCompleted: false,
-  description: "Walk the dog",
-};
+let tareas = require("./tareas.json");
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/tareas", (req, res) => {
   res.json(tareas);
+});
+
+app.post("/nuevaTarea", (req, res) => {
+  const nuevaTarea = req.body;
+  tareas.push(nuevaTarea);
+  console.log(nuevaTarea);
+
+  res.send("Tarea creada");
 });
 
 app.listen(port, () => {
